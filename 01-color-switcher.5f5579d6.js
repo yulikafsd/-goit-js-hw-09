00089e285cdfb195e51f9c290cc2035a3367529d
yulikafsd <ju.zagorovsky@gmail.com>
2022-09-28 19:42:21 +0000

    Deploying to gh-pages from @ yulikafsd/goit-js-hw-09@8b73ddefee5405abba85f8cb460ece68086cc638 🚀

---
+++ 01-color-switcher.5f5579d6.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.5f5579d6.js.map
