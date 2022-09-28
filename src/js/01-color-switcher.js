8b73ddefee5405abba85f8cb460ece68086cc638
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-09-28 21:41:48 +0200

    adds task 1 n styles

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', changeBodyColor);
+refs.stopBtn.addEventListener('click', isDisabled);
+
+function changeBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  isEnabled();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function isEnabled() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function isDisabled() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
