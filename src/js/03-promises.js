42499ca620b6245d8919f04bb81cb6adf1e02509
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-10-03 00:51:36 +0200

    adds task 3 with promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,41 +1,87 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refs = {
-  formEl: document.querySelector('.form'),
-  buttonEl: document.querySelector('button'),
-};
+const form = document.querySelector('.form');
+const submitBtn = document.querySelector('button');
 
-refs.formEl.addEventListener('submit', onSubmit);
+form.elements.delay.value = 500;
+form.elements.step.value = 1000;
+form.elements.amount.value = 3;
+
+form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-  let delay = Number(refs.formEl.elements.delay.value);
-  const step = Number(refs.formEl.elements.step.value);
-  const amount = Number(refs.formEl.elements.amount.value);
-  const delaysArr = [];
-
-  for (let position = 0; position < amount; position += 1) {
-    setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        Notify.success(
-          `Fulfilled promise ${position + 1} in ${delaysArr[position]}ms`
-        );
-      } else {
-        Notify.failure(
-          `Rejected promise ${position + 1} in ${delaysArr[position]}ms`
-        );
-      }
-    }, delay);
-    delaysArr.push(delay);
-    delay += step;
-  }
+  submitBtn.disabled = true;
+  createPromises(e);
 }
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-//   });
+function createPromises({ target: { delay, step, amount } }) {
+  let delayVal = Number(delay.value);
+  const stepVal = Number(step.value);
+  const amountVal = Number(amount.value);
+
+  for (let i = 1; i <= amountVal; i += 1, delayVal += stepVal) {
+    createPromise(i, delayVal).then(onSuccess).catch(onFailure);
+  }
+
+  activateBtn(delayVal);
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSuccess({ position, delay }) {
+  Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onFailure({ position, delay }) {
+  Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+}
+
+function activateBtn(delay) {
+  setTimeout(delay => {
+    submitBtn.disabled = false;
+  }, delay);
+}
+
+// Решение задачи без промисов :)
+
+// function onSubmit(e) {
+//   e.preventDefault();
+
+//   const { delay, step, amount } = e.target;
+
+//   let delayVal = Number(delay.value);
+//   const stepVal = Number(step.value);
+//   const amountVal = Number(amount.value);
+//   const delaysArr = [];
+
+//   for (let position = 0; position < amountVal; position += 1) {
+//     setTimeout(() => {
+//       const shouldResolve = Math.random() > 0.3;
+//       if (shouldResolve) {
+//         Notify.success(
+//           `Fulfilled promise ${position + 1} in ${delaysArr[position]}ms`
+//         );
+//       } else {
+//         Notify.failure(
+//           `Rejected promise ${position + 1} in ${delaysArr[position]}ms`
+//         );
+//       }
+//     }, delayVal);
+//     delaysArr.push(delayVal);
+//     delayVal += stepVal;
+//   }
+// }
